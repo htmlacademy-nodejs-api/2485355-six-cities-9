03c7e78a0b2760f4ba443fadc2550cb624eee6db
Kirill Nan <nan1911.work@gmail.com>
2025-02-15 20:07:35 +0500

    add import command base

---
+++ src/cli/commands/import.command.ts
@@ -0,0 +1,12 @@
+import { Command } from './command.interface.js';
+
+export class ImportCommand implements Command {
+  public getName(): string {
+    return '--import';
+  }
+
+  public async execute(...parameters: string[]): Promise<void> {
+    throw new Error('Method not implemented.');
+  }
+
+}
