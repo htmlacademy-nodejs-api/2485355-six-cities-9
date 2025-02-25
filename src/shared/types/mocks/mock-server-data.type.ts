62d17b4b5e85adb1d6c89a614f3b8b3e3e09ff15
Kirill Nan <nan1911.work@gmail.com>
2025-02-25 23:26:35 +0500

    add mock server data type

---
+++ src/shared/types/mocks/mock-server-data.type.ts
@@ -0,0 +1,34 @@
+import { Amenitie } from '../offer/amentie.type.js';
+import { City } from '../offer/city.type.js';
+import { Coordinates } from '../offer/coordinates.type.js';
+import { HousingType } from '../offer/housing-type.type.js';
+import { Photo } from '../offer/photo.type.js';
+import { UserType } from '../user/user-type.type.js';
+import { User } from '../user/user.type.js';
+
+export type MockServerData = {
+  texts: string[];
+  createdAts: string[];
+  ratings: number[];
+  authors: User[];
+  titles: string[];
+  descriptions: string[];
+  publicationDate: string[];
+  cities: City[];
+  previewImage: string[];
+  photos: Photo[][];
+  isPremium: boolean[];
+  isFavorite: boolean[];
+  housingTypes: HousingType[];
+  roomCounts: number[];
+  guestCounts: number[];
+  rentalPrices: number[];
+  amenities: Amenitie[][];
+  commentCounts: number[];
+  coordinates: Coordinates[];
+  names: string[];
+  emails: string[];
+  avatars: string[];
+  passwords: string[];
+  types: UserType[];
+};
--- src/shared/types/offer/offer.type.ts
+++ src/shared/types/offer/offer.type.ts
@@ -3,6 +3,7 @@ import { HousingType } from './housing-type.type.js';
 import { Amenitie } from './amentie.type.js';
 import { Coordinates } from './coordinates.type.js';
 import { Photo } from './photo.type.js';
+import { User } from '../user/user.type.js';
 
 export type Offer = {
   title: string;
@@ -19,7 +20,7 @@ export type Offer = {
   guestCount: number;
   rentalPrice: number;
   amenities: Amenitie[];
-  author: string;
+  author: User;
   commentCount: number;
   coordinates: Coordinates;
 };
