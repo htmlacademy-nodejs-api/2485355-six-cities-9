9d65f80b1ac825d10ae255625dd2bddb705d41e7
Kirill Nan <nan1911.work@gmail.com>
2025-03-04 00:36:27 +0500

    add database connection

--- src/shared/types/component/component.enum.ts
+++ src/shared/types/component/component.enum.ts
@@ -1,5 +1,6 @@
 export const Component = {
   RestApplication: Symbol.for('RestApplication'),
   Logger: Symbol.for('Logger'),
-  Config: Symbol.for('COnfig')
+  Config: Symbol.for('Config'),
+  DatabaseClient: Symbol.for('DatabaseClient')
 } as const;
